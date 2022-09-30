a33b08bee7c27195c43d45ce12bfd5c57976766b
JomberR <60245648+JomberR@users.noreply.github.com>
2022-09-30 17:04:50 -0500

    All buttons now work

--- web_components/faction/clock.js
+++ web_components/faction/clock.js
@@ -31,6 +31,48 @@ class Clock extends HTMLElement{
         }
     }
 
+    async setClockDisplay(){
+        let clockSize = this.shadowRoot.getElementById("clock-size");
+        let clockProgress = this.shadowRoot.getElementById("clock-progress");
+        let clockDisplay = this.shadowRoot.getElementById("clock-display");
+
+        if(parseInt(clockProgress.innerHTML) > clockSize.value){
+            clockProgress.innerHTML = clockSize.value;
+        }
+
+        clockDisplay.innerHTML = `${clockProgress.innerHTML} / ${clockSize.value}`;
+    }
+
+    clockProgressListener(){
+        let clockSize = this.shadowRoot.getElementById("clock-size");
+        let clockProgress = this.shadowRoot.getElementById("clock-progress");
+        let clockAdd = this.shadowRoot.getElementById("clock-add");
+        let clockSubtract = this.shadowRoot.getElementById("clock-subtract");
+
+        const setClockDisplayBind = this.setClockDisplay.bind(this);
+
+        clockSize.addEventListener("change", setClockDisplayBind);
+
+        clockAdd.addEventListener("click", function(){
+            let progress = parseInt(clockProgress.innerHTML);
+            if(progress < clockSize.value){
+                progress++;
+                clockProgress.innerHTML = progress;
+                setClockDisplayBind();
+            }
+        });
+
+        clockSubtract.addEventListener("click", function(){
+            let progress = parseInt(clockProgress.innerHTML);
+            if(progress > 0){
+                progress--;
+                clockProgress.innerHTML = progress;
+                setClockDisplayBind();
+            }
+        });
+        
+    }
+
     clockNameListener(){
         let clock = this.shadowRoot.getElementById("clock-name");
         let input = this.shadowRoot.getElementById("clock-name-input");
@@ -56,15 +98,17 @@ class Clock extends HTMLElement{
         });
     }
 
-    setListeners(){
+    async setListeners(){
         this.clockNameListener();
         this.deleteListener();
+        this.clockProgressListener();
     }
 
     async init(){
         const shadow = this.attachShadow({mode: "open"});
         shadow.innerHTML = await this.fetchTemplate();
         this.setListeners();
+        this.setClockDisplay();
     }
 
     connectedCallback(){
